78653dee36c2e51ba2c65d8de780a5ad6563cfb9
anhphung17123 <anhhna17710110@gmail.com>
2025-07-15 15:21:34 +0700

    feat: show trending movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,95 @@
 import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  ScrollView,
-  Text,
   View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  Image,
+  FlatList,
 } from "react-native";
-import { images } from "@/constants/images";
-import { icons } from "@/constants/icons";
-import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
-import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
-export default function Index() {
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+import SearchBar from "@/components/SearchBar";
+import MovieCard from "@/components/MovieCard";
+import TrendingCard from "@/components/TrendingCard";
+
+const Index = () => {
   const router = useRouter();
-  const { data: movies, error, isLoading, reset, refetch } = useFetch(
-    () => fetchMovies({ query: "" })
-  );
-  console.log(movies);
+
+  const {
+    data: trendingMovies,
+    isLoading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
+
+  const {
+    data: movies,
+    isLoading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="w-full absolute z-0" />
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+
       <ScrollView
         className="flex-1 px-5"
-        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
         showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        {isLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : error ? (
-          <Text>Error: {error.message}</Text>
-        ): (
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
+        ) : (
           <View className="flex-1 mt-5">
             <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
               placeholder="Search for a movie"
-              onPress={() => router.push("/search")}
             />
 
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
+              </View>
+            )}
+
             <>
-              <Text className="text-lg text-white mt-5 mb-3 font-bold">
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movies
               </Text>
 
@@ -70,4 +113,6 @@ export default function Index() {
       </ScrollView>
     </View>
   );
-}
+};
+
+export default Index;
